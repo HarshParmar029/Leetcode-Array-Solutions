339281d9dc55b2364207678f56239e2cb8d92af5
029_HarshParmar <hp259369@gmail.com>
2026-08-16 21:52:52 +0530

    Implement BrowserHistory class with visit, back, and forward

---
+++ problem_1472_Design Browser History.cpp
@@ -0,0 +1,33 @@
+class BrowserHistory {
+public:
+    vector<string> history;
+    int current;
+
+    BrowserHistory(string homepage) {
+        history.push_back(homepage);
+        current = 0;
+    }
+    
+    void visit(string url) {
+        // Remove forward history
+        history.resize(current + 1);
+
+        // Add new URL
+        history.push_back(url);
+
+        // Move to new page
+        current++;
+    }
+    
+    string back(int steps) {
+        current = max(0, current - steps);
+
+        return history[current];
+    }
+    
+    string forward(int steps) {
+        current = min((int)history.size() - 1, current + steps);
+
+        return history[current];
+    }
+};
