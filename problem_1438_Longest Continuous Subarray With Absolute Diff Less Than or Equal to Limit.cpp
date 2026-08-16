7dec1879b1472ae08d809cfb4c06c805d01659ec
029_HarshParmar <hp259369@gmail.com>
2026-08-16 22:09:18 +0530

    Add solution for longest subarray with absolute diff limit
    Implement a solution to find the longest continuous subarray with absolute difference less than or equal to a given limit using deques.

---
+++ problem_1438_Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums, int limit) {
+        
+        deque<int> maxD;
+        deque<int> minD;
+        
+        int left = 0;
+        int ans = 0;
+        
+        for (int right = 0; right < nums.size(); right++) {
+            
+            // Maintain decreasing deque for maximum
+            while (!maxD.empty() && nums[maxD.back()] < nums[right]) {
+                maxD.pop_back();
+            }
+            maxD.push_back(right);
+            
+            // Maintain increasing deque for minimum
+            while (!minD.empty() && nums[minD.back()] > nums[right]) {
+                minD.pop_back();
+            }
+            minD.push_back(right);
+            
+            // If window is invalid, shrink it
+            while (nums[maxD.front()] - nums[minD.front()] > limit) {
+                
+                if (maxD.front() == left) {
+                    maxD.pop_front();
+                }
+                
+                if (minD.front() == left) {
+                    minD.pop_front();
+                }
+                
+                left++;
+            }
+            
+            ans = max(ans, right - left + 1);
+        }
+        
+        return ans;
+    }
+};
