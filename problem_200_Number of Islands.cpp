3ce8d5a9a415cc4fd356f8b530fc9c4fdb666ef9
029_HarshParmar <hp259369@gmail.com>
2026-08-16 19:45:02 +0530

    Implement DFS to count number of islands

---
+++ problem_200_Number of Islands.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+
+    void dfs(vector<vector<char>>& grid, int i, int j) {
+
+        int m = grid.size();
+        int n = grid[0].size();
+
+        // Boundary check
+        if (i < 0 || i >= m || j < 0 || j >= n) {
+            return;
+        }
+
+        // Agar water hai ya already visited hai
+        if (grid[i][j] == '0') {
+            return;
+        }
+
+        // Is land ko visited mark karo
+        grid[i][j] = '0';
+
+        // Up
+        dfs(grid, i - 1, j);
+
+        // Down
+        dfs(grid, i + 1, j);
+
+        // Left
+        dfs(grid, i, j - 1);
+
+        // Right
+        dfs(grid, i, j + 1);
+    }
+
+    int numIslands(vector<vector<char>>& grid) {
+
+        int m = grid.size();
+        int n = grid[0].size();
+
+        int count = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (grid[i][j] == '1') {
+
+                    // New island found
+                    count++;
+
+                    // Puri island ko visit karo
+                    dfs(grid, i, j);
+                }
+            }
+        }
+
+        return count;
+    }
+};
