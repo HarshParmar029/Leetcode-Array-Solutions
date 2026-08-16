4a5ea17ee4f36eb498d7cbf9b4822493ee98031d
029_HarshParmar <hp259369@gmail.com>
2026-08-16 20:43:07 +0530

    Implement combinationSum4 function for target sums

---
+++ Problem_377-Combination Sum IV.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+
+        vector<long long> dp(target + 1, 0);
+
+        dp[0] = 1;
+
+        for (int i = 1; i <= target; i++) {
+
+            for (int num : nums) {
+
+                if (num <= i) {
+
+                    dp[i] += dp[i - num];
+
+                    // Prevent overflow
+                    if (dp[i] > INT_MAX) {
+                        dp[i] = INT_MAX;
+                    }
+                }
+            }
+        }
+
+        return (int)dp[target];
+    }
+};
